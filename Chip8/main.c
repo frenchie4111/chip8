ef5cfd892d74b6371410a2c05ce503bce45e240c
Mike Lyons <mdl0394@gmail.com>
2014-08-23 12:52:39 -0700

    Initial Commit

---
+++ Chip8/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chip8
+//
+//  Created by Mike Lyons on 8/23/14.
+//  Copyright (c) 2014 Mike Lyons. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
